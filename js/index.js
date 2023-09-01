581f514fc8592947b29b5f41708481404bae1e46
Ricardo Brito Brens <britobrensricardo@gmail.com>
2023-08-31 23:46:33 -0400

    [REM] comment code

--- js/index.js
+++ js/index.js
@@ -12,38 +12,9 @@ function setVisibilityToDiv(divId, visibility) {
 }
 
 function doTheMagic() {
-
-    const smallWidth = 500;
-    const smallHeight = 1000;
-
-    if (window.innerWidth < smallWidth && window.innerHeight < smallHeight) {
-        setVisibilityToDiv('social-media-horizontal-list', "hide");
-    }
-
-    if (window.innerWidth == 688 && window.innerHeight == 1031) {
+    if (window.innerWidth <= 991) {
         setVisibilityToDiv('social-media-horizontal-list', "hide");
     }
 }
 
 doTheMagic();
-//setVisibilityToDiv('social-media-horizontal-list', "hide");
-
-
-// window.addEventListener("resize", function () { console.log('resize!') }, true);
-
-// if (window.attachEvent) {
-//     window.attachEvent('onresize', function () {
-//         alert('attachEvent - resize');
-//         doTheMagic();
-//     });
-// }
-// else if (window.addEventListener) {
-//     window.addEventListener('resize', function () {
-//         console.log('addEventListener - resize');
-//         doTheMagic();
-//     }, true);
-// }
-// else {
-//     //The browser does not support Javascript event binding
-// }
-
